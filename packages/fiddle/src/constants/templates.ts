ea9fbc64aa4e54a62882e768d4d23eaad3ad7563
Raymond Muller <raymond.muller.1989@gmail.com>
2022-09-24 00:25:26 +0200

    fix(fiddle): update loop template

--- packages/fiddle/src/constants/templates.ts
+++ packages/fiddle/src/constants/templates.ts
@@ -45,66 +45,16 @@ export const templates: { [key: string]: string } = {
         Show
       </button>
     {/if}
-  
-
-
   `,
   loop: dedent`
-    import { useStore } from "@builder.io/mitosis";
-    
-    export default function MyComponent(props) {
-      const state = useStore({
-        list: ["hello", "world"],
-        newItemName: "New item",
-        addItem() {
-          state.list = [
-            ...state.list, 
-            state.newItemName,
-          ]
-        }
-      })
-    
-      return (
-        <div
-          css={{
-            padding: "10px",
-          }}
-        >
-          <link
-            href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-            rel="stylesheet"
-          />
-    
-          <input
-            class="shadow-md rounded w-full px-4 py-2"
-            value={state.newItemName}
-            onChange={(event) => {state.newItemName = event.target.value}}
-          />
-    
-          <button
-            class="bg-blue-500 rounded w-full text-white font-bold py-2 px-4 "
-            css={{
-              margin: "10px 0",
-            }}
-            onClick={(event) => state.addItem()}
-          >
-            Add list item
-          </button>
-    
-          <div class="shadow-md rounded">
-            {state.list.map((item) => (
-              <div
-                class="border-gray-200 border-b"
-                css={{
-                  padding: "10px",
-                }}
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-        </div>
-      );
-    }
+  <script>
+    let numberse = ['one', 'two', 'three'];
+  </script>
+
+  <ul>
+    {#each numbers as num}
+      <li>{num}</li>
+    {/each}
+  </ul>
   `,
 };

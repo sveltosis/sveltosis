05c876c2467b210fdc2a7ad48b25b030d8f79e5e
Raymond Muller <raymond.muller.1989@gmail.com>
2022-09-25 14:18:28 +0200

    fix(parser): add logic to convert more complex refs (containing other refs)

--- packages/parser/src/instance/refs.ts
+++ packages/parser/src/instance/refs.ts
@@ -1,12 +1,17 @@
 import { generate } from 'astring';
+import { isString, some } from 'lodash';
 import { possiblyAppendPropsOrState } from '../helpers/bindings';
 
 function getParsedValue(json: SveltosisComponent, el: any) {
-  if (el.type === "Identifier") {
+  if (el.type === 'Identifier') {
     return possiblyAppendPropsOrState(json, el.name);
   } else {
     return possiblyAppendPropsOrState(json, el.value);
   }
+}
+
+function isPropOrStateRef(i: any) {
+  return isString(i) && (i.indexOf('props.') >= 0 || i.indexOf('state.') >= 0);
 }
 
 export function parseRefs(json: SveltosisComponent, node: any) {
@@ -14,11 +19,25 @@ export function parseRefs(json: SveltosisComponent, node: any) {
 
   let code;
 
-  if (declaration.init.type === "ArrayExpression") {
+  if (declaration.init.type === 'ArrayExpression') {
     code = declaration.init.elements.map((el: any) => {
       return getParsedValue(json, el);
     });
-  } else if (declaration.init.type === "ObjectExpression") {
+
+    if (some(code, (i: string) => isPropOrStateRef(i))) {
+      let name = declaration.id.name;
+      json.state[name] = {
+        code: `get ${name}() { return [${code.map((i: any) => {
+          if (isPropOrStateRef(i)) {
+            return i;
+          }
+          return JSON.stringify(i);
+        })}]}`,
+        type: 'getter',
+      };
+      return;
+    }
+  } else if (declaration.init.type === 'ObjectExpression') {
     code = declaration.init.properties
       .map((el: any) => {
         return {

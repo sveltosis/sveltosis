3b7cf04a6e034ac90cb759648f95129d4bb30b43
Raymond Muller <raymond.muller.1989@gmail.com>
2022-09-25 03:02:55 +0200

    fix(parser): simplify refs (todo: arrays should not be parsed as a string)

--- packages/parser/src/instance/refs.ts
+++ packages/parser/src/instance/refs.ts
@@ -1,7 +1,13 @@
+import { generate } from "astring";
+import { possiblyAppendPropsOrState } from "../helpers/bindings";
+
 export function parseRefs(json: SveltosisComponent, node: any) {
   const declaration = node.declarations[0];
+
+  let code = possiblyAppendPropsOrState(json, generate(declaration.init));
+
   json.state[declaration.id.name] = {
-    code: declaration.init.value,
+    code,
     type: "property",
   };
 }

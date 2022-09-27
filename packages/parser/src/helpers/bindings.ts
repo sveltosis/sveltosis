4cfa470289225e0b557a7ca99f09fa482afd5d85
Raymond Muller <raymond.muller.1989@gmail.com>
2022-09-27 20:42:49 +0200

    chore(parser): add todo

--- packages/parser/src/helpers/bindings.ts
+++ packages/parser/src/helpers/bindings.ts
@@ -4,6 +4,10 @@ export function possiblyAppendPropertiesOrState(json: SveltosisComponent, input:
   const propertyKeys = Object.keys(json.props);
   const stateKeys = Object.keys(json.state);
 
+  // todo: we are currently also replacing name in the log string
+  // and we probably want to avoid this
+  // if (name) { console.log('has a name') }
+
   for (const property of propertyKeys) {
     const regex = new RegExp(`(props.)?${property}\\b`, 'g');
     if (regex.test(output)) {

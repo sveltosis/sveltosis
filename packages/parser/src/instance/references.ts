0dbb319444af5c35ec6a6d05430adf9eee6aa8f0
Raymond Muller <raymond.muller.1989@gmail.com>
2022-11-07 11:51:39 +0100

    fix(parser): object expressions

--- packages/parser/src/instance/references.ts
+++ packages/parser/src/instance/references.ts
@@ -25,52 +25,53 @@ export function parseReferences(json: SveltosisComponent, node: VariableDeclarat
   let type: any = 'property';
 
   switch (declaration?.init?.type) {
-  case 'ArrayExpression': {
-    code = declaration.init.elements.map((element) => {
-      return getParsedValue(json, element as Expression);
-    });
+    case 'ArrayExpression': {
+      code = declaration.init.elements.map((element) => {
+        return getParsedValue(json, element as Expression);
+      });
 
-    if (some(code, (index: string) => isPropertyOrStateReference(index))) {
-      const name = (declaration.id as Identifier).name;
-      json.state[name] = {
-        code: `get ${name}() { return [${code.map((index) => {
-          if (isPropertyOrStateReference(index)) {
-            return index;
-          }
-          return JSON.stringify(index);
-        })}]}`,
-        type: 'getter',
-      };
-      return;
-    }
-  
-  break;
-  }
-  case 'ObjectExpression': {
-    const properties = declaration.init.properties.map((element) => {
-      const element_ = element as Property;
-      return {
-        [generate(element_.key)]: getParsedValue(json, element_.value),
-      };
-    });
+      if (some(code, (index: string) => isPropertyOrStateReference(index))) {
+        const name = (declaration.id as Identifier).name;
+        json.state[name] = {
+          code: `get ${name}() { return [${code.map((index) => {
+            if (isPropertyOrStateReference(index)) {
+              return index;
+            }
+            return JSON.stringify(index);
+          })}]}`,
+          type: 'getter',
+        };
+        return;
+      }
 
-    code = {};
-    for (const item of properties) {
-      Object.assign(code, { [item.key]: item.value });
+      break;
     }
-  
-  break;
-  }
-  case 'FunctionExpression': {
-    declaration.init.id = declaration.id as Identifier;
-    code = generate(declaration.init);
-    type = 'function';
-  
-  break;
-  }
-  default: {
-    code = (declaration?.init as SimpleLiteral)?.value as string;
-  }
+    case 'ObjectExpression': {
+      const properties = declaration.init.properties.map((element) => {
+        const element_ = element as Property;
+        return {
+          key: generate(element_.key),
+          value: getParsedValue(json, element_.value),
+        };
+      });
+
+      code = {};
+      for (const item of properties) {
+        Object.assign(code, { [item.key]: item.value });
+      }
+
+      break;
+    }
+    case 'FunctionExpression': {
+      declaration.init.id = declaration.id as Identifier;
+      code = generate(declaration.init);
+      type = 'function';
+
+      break;
+    }
+    default: {
+      code = (declaration?.init as SimpleLiteral)?.value as string;
+    }
   }
 
   json.state[(declaration.id as Identifier).name] = {
